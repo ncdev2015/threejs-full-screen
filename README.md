4a9123715a0970cdcc3327a13becef60149d2b3a
Nelson C <ncdev2015@gmail.com>
2024-03-30 05:21:08 -0300

    update main.ts && add README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# threejs-full-screen
+
+This example shows you how to get a full screen mode window from the canvas content by clicking a button
+
+## Requirements
+
+* Nodejs v18+
+* Vite
+* Three
+
+## Install
+
+* npm install
+
+## Run for development
+
+* npm run dev
+
+## Compile for production
+
+* npm run build
+
+## Examples
--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,16 @@ import { setupScene } from './scene.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Full Screen test</h1>
-
     <button id='fullscreen-btn'>Full screen</button>
     <br/>
     <br/>
-    <div id='viewer'></div>    
-
+    <div id='viewer'></div>
   </div>
 `
 
 document.getElementById('fullscreen-btn')!.addEventListener('click', function() {
-  var canvas = document.querySelector('canvas')!;
-
-  if (canvas.requestFullscreen) {
-    canvas.requestFullscreen();
-  }
-  
+  let canvas = document.querySelector('canvas')!;
+  canvas.requestFullscreen && canvas.requestFullscreen();  
 });
 
 setupScene("viewer");
